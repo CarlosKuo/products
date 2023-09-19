0710604a5d6e34838402b8dae6757570f0ff3aeb
CarlosKuo <yihsiang731117@gmail.com>
2023-09-19 16:07:04 +0800

    refactor

--- products.py
+++ products.py
@@ -1,13 +1,10 @@
 import os # operating system
 
-products = []
-# 相對路徑: 目前程式當下的資料夾位置
-if os.path.isfile('products.csv'): # 檢查檔案在不在
-    print('year!找到檔案了!')
-    # 讀取檔案
-    with open('products.csv', 'r', encoding = 'utf-8') as f:
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding = 'utf-8') as f:
         for line in f:
-            #    strip() 去除換行和空格  split() 字串切割，切割完的結果是清單(List)
+            # strip() 去除換行和空格  split() 字串切割，切割完的結果是清單(List)
             # s = line.strip().split(',')
             # name = s[0]
             # price = s[1]
@@ -16,31 +13,47 @@ if os.path.isfile('products.csv'): # 檢查檔案在不在
                 continue    # 繼續(我解釋成忽略(跳過)當下動作)
             name, price = line.strip().split(',')
             products.append([name, price])
-    print(products)
-else:
-    print('找不到檔案 ...')
+    return products
 
 # 讓使用者輸入
-while True:
-    name = input('請輸入商品名稱: ')
-    if name == 'q':
-        break
-    price = input('請輸入商品價格: ')
-    # p = []
-    # p.append(name)
-    # p.append(price)
-    # p = [name, price]
-    # products.append(p)
-    products.append([name, price])
-
-print(products)
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱: ')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格: ')
+        # p = []
+        # p.append(name)
+        # p.append(price)
+        # p = [name, price]
+        # products.append(p)
+        products.append([name, price])
+    print(products)
+    return products
 
 # 印出所有購買紀錄
-for p in products:
-    print(p[0], '的價格是', p[1])
+def print_products(products):
+    for p in products:
+        print(p[0], '的價格是', p[1])
 
 # 寫入檔案
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-    f.write('商品,價格\n')
-    for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename, products):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        f.write('商品,價格\n')
+        for p in products:
+            f.write(p[0] + ',' + p[1] + '\n')
+
+def main():
+    filename = 'products.csv'
+    # 相對路徑: 目前程式當下的資料夾位置
+    if os.path.isfile(filename): # 檢查檔案在不在
+        print('year!找到檔案了!')
+        products = read_file(filename)
+    else:
+        print('找不到檔案 ...')
+
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+main()
